b2a867fca36a6085c247f782ba4630355019df7a
Guzel <hamzina0092@gmail.com>
2020-04-25 15:52:19 +0300

    second task

--- sandBox/src/main/java/MyFirstProgram.java
+++ sandBox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,28 @@
+import firstTask.Point;
+
+import static java.awt.geom.Point2D.distance;
+import static java.awt.geom.Point2D.distanceSq;
+
 public class MyFirstProgram{
 
 	public static void main(String[] args){
 		System.out.println("hello, world!");
-	
+		System.out.println("------------");
+
+
+
+		double x1 = 3;
+		double y1 = 4;
+		double x2 = 7;
+		double y2 = 1;
+		Point p = new Point( x1, y1, x2, y2);
+		System.out.println("Distance between two points: " + p.distance());
+		System.out.println("------------");
+
 	}
+
+
+
+
 
 }
