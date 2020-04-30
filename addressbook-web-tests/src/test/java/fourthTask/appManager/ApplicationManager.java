2617aabf7793314005513ae9ebff345c2406fb78
Guzel <hamzina0092@gmail.com>
2020-04-30 20:24:59 +0300

    Common HelperBase class was created for helper classes

--- addressbook-web-tests/src/test/java/fourthTask/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/fourthTask/appManager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package fourthTask.appManager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -38,14 +37,6 @@ public class ApplicationManager {
         }
     }
 
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
