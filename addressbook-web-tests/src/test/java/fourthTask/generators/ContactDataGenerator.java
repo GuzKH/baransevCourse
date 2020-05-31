d8a42018240387de517e3eede0d19f8e5b879525
Guzel <hamzina0092@gmail.com>
2020-05-31 21:25:47 +0300

    6.3 for both parts

--- addressbook-web-tests/src/test/java/fourthTask/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/fourthTask/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package fourthTask.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import fourthTask.model.ContactData;
 
 import java.io.File;
@@ -10,15 +13,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        List<ContactData> contacts = generateContact(count);
-        save(contacts, file);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+
+//        int count = Integer.parseInt(args[0]);
+//        File file = new File(args[1]);
     }
 
-    private static void save(List<ContactData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContact(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(" . ").getAbsolutePath());
 
         Writer writer = new FileWriter(file);
@@ -33,14 +56,14 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactData> generateContact(int count) {
+    private List<ContactData> generateContact(int count) {
         List<ContactData> contacts = new ArrayList<ContactData>();
         for (int i = 0; i < count; i++) {
-            contacts.add(new ContactData().withFirstName(String.format("test %s", i))
-                    .withLastName(String.format("test %s", i))
-                    .withAddress(String.format("test %s", i))
-                    .withEmail(String.format("test@%s", i))
-                    .withAllPhones(String.format("981111111%s", i)));
+            contacts.add(new ContactData().withFirstName(String.format("First name: test %s", i))
+                    .withLastName(String.format(" Last name: test %s", i))
+                    .withAddress(String.format(" Address: test %s", i))
+                    .withEmail(String.format(" Email: test@%s", i))
+                    .withAllPhones(String.format(" Phone(s): 981111111%s", i)));
         }
         return contacts;
     }
